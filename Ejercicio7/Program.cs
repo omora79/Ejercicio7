fd0cc3da4cc7f893fe596cee06bd722046c140e9
OSCAR ALBERTO MORA M�NDEZ <oscarmora18268@correo.itm.edu.co>
2023-09-01 19:38:44 -0500

    Programa terminado

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,37 @@
+﻿// Escribir un algoritmo que dados 2 valores de entrada imprima siempre la división del mayor entre el menor.
+
+
+// 1 clase y metodo
+
+public class Ejercicio7
+{
+    static void Main(string[] args)
+    {
+        // declaracion de variables
+
+        double num1, num2, divi;
+
+        // 3 pedir datos por panatalla
+
+        Console.WriteLine("Ingrese el primer numero: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Ingrese el segundo numero: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        // 4 realizar los calculos
+        if (num1 > num2)
+        {
+            divi = num1 / num2;
+        }
+        else divi = num2 / num1;
+
+        // mostrar resultados en pantalla
+
+        Console.WriteLine($"la divicion entre el numero mayor con respecto a menor es: {divi}");
+
+
+
+
+    }
+}
